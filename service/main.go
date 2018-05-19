433d74fef6665f1418604ff29f06668a3e615a27
HanmingLi <hanming@wustl.edu>
2018-05-19 17:34:36 -0500

    bigtable added1

--- service/main.go
+++ service/main.go
@@ -29,7 +29,7 @@ const (
 	PROJECT_ID = "around-204013"
 	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.229.66.177:9200"
+	ES_URL = "http://35.231.163.68:9200"
 	BUCKET_NAME = "post-images-204013"
 )
 
